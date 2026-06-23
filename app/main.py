6bcffe0101be4752b3e9a9b8c344dabcaa5b2f49
Hadi Saleemi <hadisaleemi2005@gmail.com>
2026-06-23 23:53:59 +0500

    disable debug mode

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 import sys
 import os
 
-
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
     pass
+
+def getFirstWordEndIndex(command):
+        index = command.find(" ")
+        index = index + 1
+        return index
 
 
 if __name__ == "__main__":
@@ -13,33 +17,26 @@ if __name__ == "__main__":
         builtinCommands = ["echo", "type", "exit"]
         main()
         command = input()
+        index = getFirstWordEndIndex()
         if (command == "exit"):
             break
-        elif ("echo" in command[:len("echo")]):
-            index = command.find(" ")
-            index = index + 1
+        elif ("echo" in command[:index]):
             print(command[index:])
-        elif ("type" in command[:len("type")]):
-            index = command.find(" ")
-            index = index + 1
+        elif ("type" in command[:index]):
             if (command[index:] in builtinCommands):
                 print(f"{command[index:]} is a shell builtin")
             else:
                 system_path = os.environ.get('PATH')
-                commandIndex = command.find(" ")
-                commandIndex = commandIndex + 1
-                ogcommand = command[commandIndex:] 
-                command = command[commandIndex:] + ".exe"
-                directories = system_path.split(":")
+                command = command[index:] 
+                directories = system_path.split(os.pathsep)
+                print(directories)
                 found = False
-                for directory in directories:
-                    if os.path.exists(directory):
-                        print(os.listdir(directory))
+                for directory in directories: 
                     if os.path.exists(directory) and command in os.listdir(directory):
                         found = True
-                        print(f"{ogcommand} is {directory}")
+                        print(f"{command} is {directory}{os.path.sep}{command}")
                         break
                 if (not found):
-                    print(f"{ogcommand}: not found")
+                    print(f"{command}: not found")
         else:
             print(f"{command}: command not found")
