4de49ec420f5e97de5c8b9394da07518f16d1c54
hadi <hadisaleemi2005@gmail.com>
2026-06-24 14:38:05 +0500

    refactor code

--- app/main.py
+++ app/main.py
@@ -2,72 +2,57 @@ import sys
 import os
 import subprocess
 
-def getCommandAndArguementIndexes(command):
-        index = command.find(" ")
-        index = index + 1
-        if (index == 0):
-            return len(command), -1
-        return index - 1, index
-
-def getExecutablePath(command):
+def getExecutablePath(executable):
     system_path = os.environ.get('PATH')
     directories = system_path.split(os.pathsep)
     found = False
     path = ""
     for directory in directories: 
-        if os.path.exists(directory) and command in os.listdir(directory) and os.access(f"{directory}{os.path.sep}{command}", os.X_OK):
+        if os.path.exists(directory) and executable in os.listdir(directory) and os.access(f"{directory}{os.path.sep}{executable}", os.X_OK):
             found = True
-            path = directory + os.path.sep + command
+            path = directory + os.path.sep + executable
             return found, path
     return found, path
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-     builtinCommands = ["echo", "type", "exit", "pwd"]
+     builtinCommands = ["echo", "type", "exit", "pwd", "cd"]
+     specialQuotes = ["'", '"']
      while (True):
         sys.stdout.write("$ ")
-        command = input()
-        endOfCommand, startOfArguements = getCommandAndArguementIndexes(command)
-        if (command == "exit"):
+        userInput = input()
+        userInput = userInput.split()
+        arguments = userInput[1:]
+        command = userInput[:1]
+        if (command[0] == "exit"):
             break
-        elif ("echo" in command[:endOfCommand]):
-            if (startOfArguements != -1):    
-                command = command[startOfArguements:]
-                if ("'" not in command):
-                    command = command.split()
-                    command = " ".join(command)
-                else:
-                    command = command.split("'")
-                    command = "".join(command)
-                print(command)
+        elif (command[0] == "echo"):
+            print(' '.join(arguments) if len(arguments) > 0 else "")
+        elif (command[0] == "type"):
+            if (arguments[0] in builtinCommands):
+                print(f"{" ".join(arguments)} is a shell builtin")
             else:
-                print("")
-        elif ("type" in command[:endOfCommand]):
-            if (command[startOfArguements:] in builtinCommands):
-                print(f"{command[startOfArguements:]} is a shell builtin")
-            else:
-                command = command[startOfArguements:] 
-                found, path = getExecutablePath(command)
+                found, path = getExecutablePath(" ".join(arguments))
                 if (found):
-                    print(f"{command} is {path}")
+                    print(f"{" ".join(arguments)} is {path}")
                 else:
-                    print(f"{command}: not found")
-        elif (command == "pwd"):
+                    print(f"{" ".join(arguments)}: not found")
+        elif (command[0] == "pwd"):
             print(os.getcwd())
-        elif ("cd" in command[:endOfCommand]):
-            path = command[startOfArguements:] if startOfArguements != -1 else ""
+        elif (command[0] == "cd"):
+            path = " ".join(arguments)
             if (path == "~"):
                 path = os.environ.get('HOME')
             if (os.path.exists(path)):
                 os.chdir(path)
             else:
-                print(f"{command[:endOfCommand]}: {path}: No such file or directory")
+                print(f"{command[0]}: {path}: No such file or directory")
         else:
-            found, path = getExecutablePath(command[:endOfCommand])
+            found, path = getExecutablePath(command[0])
             if (found):
-                subprocess.run(command, shell=True)
+                subprocess.run((command[0] + " " + " ".join(arguments)), shell=True)
             else:
-                print(f"{command[:endOfCommand]}: command not found")
+                print(f"{command[0]}: command not found")
 
 
 if __name__ == "__main__":
